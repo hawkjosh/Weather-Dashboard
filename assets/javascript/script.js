b0cf4719855a5eb53b23655bb0f475e1f392609c
Josh Hawk <hawkjosh@icloud.com>
2022-07-14 01:09:32 -0400

    HTML skeleton and started JS components

---
+++ assets/javascript/script.js
@@ -0,0 +1,14 @@
+// let inputEl = document.getElementById('input');
+// let submitEl = document.getElementById('submit');
+
+// var APIKey = 'dd6c4adf0524e9123c18fdc111337e48';
+// var city;
+
+// submitEl.addEventListener('click', searchCity);
+
+// var queryURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=5&appid=" + APIKey;
+
+// function searchCity() {
+//   city = inputEl.value;
+//   console.log(fetch(queryURL));
+// }
