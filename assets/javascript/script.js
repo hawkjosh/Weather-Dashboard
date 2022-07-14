7b591ab3730c1c821d3fac61e3b9bf03e5a81442
Josh Hawk <hawkjosh@icloud.com>
2022-07-14 17:40:32 -0400

    Began bootstrap styling in HTML, also continued with some JS functionality

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,14 +1,32 @@
-// let inputEl = document.getElementById('input');
-// let submitEl = document.getElementById('submit');
+// Set up global DOM variables
+const searchBar = document.getElementById('search-bar');
+const searchBtn = document.getElementById('search-btn');
+const history = document.getElementById('search-history');
+const city = document.getElementById('city');
+const currDate = document.getElementById('curr-date');
+const currIcon = document.getElementById('curr-icon');
+const currInfo = document.querySelectorAll('.curr-info');
+const forecastDate = document.querySelectorAll('.forecast-date');
 
-// var APIKey = 'dd6c4adf0524e9123c18fdc111337e48';
-// var city;
+function initialDisplay() {
+  let search = searchBar.value;
+  console.log(search);
+  city.innerHTML = search + ' ' + moment().format('(M/D/YYYY)');
+  currIcon.innerHTML = '☁️';
+  for (i=0; i<forecastDate.length; i++) {
+    forecastDate[i].innerHTML = moment().add(i + 1, 'days').format('M/D/YYYY');
+  }
+}
 
-// submitEl.addEventListener('click', searchCity);
+searchBtn.addEventListener('click', initialDisplay);
 
-// var queryURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=5&appid=" + APIKey;
+
+// const APIKey = 'dd6c4adf0524e9123c18fdc111337e48';
+
+// let queryURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=5&appid=" + APIKey;
+
 
 // function searchCity() {
-//   city = inputEl.value;
+//   city = searchBar.value;
 //   console.log(fetch(queryURL));
 // }
